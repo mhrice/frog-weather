81c06013accce1e409b8e2518dbc102eba0aa76d
Matthew Rice <mattricesound@gmail.com>
2024-06-20 22:07:21 -0700

    New changes for new images

--- frog.js
+++ frog.js
@@ -8,7 +8,7 @@
 // Set that ID to CITY_WEATHER
 
 //API_KEY
-let API_WEATHER = "API_KEY_HERE";
+let API_WEATHER = "b352a644447eca79e479ca6b0aa2404f";
 let CITY_WEATHER = "5391811";
 let UNIT_TYPE = "F";//C for Celius and F for fernite
 
@@ -16,7 +16,7 @@ let UNIT_TYPE = "F";//C for Celius and F for fernite
 var today = new Date();
 
 //Initlize Widget
-let widget = new ListWidget(); 
+let widget = new ListWidget();
 
 //Get storage
 var fm = FileManager.iCloud();
@@ -27,112 +27,116 @@ var textcolor = new Color("#ffffff");
 
 // Fetch Image from Url
 async function fetchimageurl(url) {
-	const request = new Request(url)
-	var res = await request.loadImage();
-	return res;
+  const request = new Request(url)
+  var res = await request.loadImage();
+  return res;
 }
 
 // Get formatted Date
-function getformatteddate(){
-  var months = ['January','February','March','April','May','June','July','August','September','October','November','December'];
+function getformatteddate() {
+  var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
   return months[today.getMonth()] + " " + today.getDate()
 }
 
 // Load image from local drive
-async function fetchimagelocal(path){
+async function fetchimagelocal(path, isBackground) {
   var finalPath = base_path + path + ".png";
-  if(fm.fileExists(finalPath)==true){
+  if (fm.fileExists(finalPath) == true) {
     console.log("file exists: " + finalPath);
     return finalPath;
-  }else{
+  } else {
     //throw new Error("Error file not found: " + path);
-    if(fm.fileExists(base_path)==false){
+    if (fm.fileExists(base_path) == false) {
       console.log("Directry not exist creating one.");
       fm.createDirectory(base_path);
     }
     console.log("Downloading file: " + finalPath);
-    await downloadimg(path);
-    if(fm.fileExists(finalPath)==true){
+    await downloadimg(path, isBackground);
+    if (fm.fileExists(finalPath) == true) {
       console.log("file exists after download: " + finalPath);
       return finalPath;
-    }else{
+    } else {
       throw new Error("Error file not found: " + path);
     }
   }
 }
 
-async function downloadimg(path){
+async function downloadimg(path, isBackground) {
+  var imgurl = null;
+  if (isBackground) {
+    imageUrl = path;
+  } else {
     const url = "https://github.com/mhrice/frog-weather/raw/main/new-frog.json";
     const data = await fetchWeatherData(url);
     var dataimg = null;
     var name = null;
-    if(path.includes("bg")){
+    if (path.includes("bg")) {
       dataimg = data.background;
-      name = path.replace("_bg","");
-    }else{
+      name = path.replace("_bg", "");
+    } else {
       dataimg = data.icon;
-      name = path.replace("_ico","");
+      name = path.replace("_ico", "");
     }
-    var imgurl=null;
-    switch (name){
+    switch (name) {
       case "01d":
         imgurl = dataimg._01d;
-      break;
+        break;
       case "01n":
         imgurl = dataimg._01n;
-      break;
+        break;
       case "02d":
         imgurl = dataimg._02d;
-      break;
+        break;
       case "02n":
         imgurl = dataimg._02n;
-      break;
+        break;
       case "03d":
         imgurl = dataimg._03d;
-      break;
+        break;
       case "03n":
         imgurl = dataimg._03n;
-      break;
+        break;
       case "04d":
         imgurl = dataimg._04d;
-      break;
+        break;
       case "04n":
         imgurl = dataimg._04n;
-      break;
+        break;
       case "09d":
         imgurl = dataimg._09d;
-      break;
+        break;
       case "09n":
         imgurl = dataimg._09n;
-      break;
+        break;
       case "10d":
         imgurl = dataimg._10d;
-      break;
+        break;
       case "10n":
         imgurl = dataimg._10n;
-      break;
+        break;
       case "11d":
         imgurl = dataimg._11d;
-      break;
+        break;
       case "11n":
         imgurl = dataimg._11n;
-      break;
+        break;
       case "13d":
         imgurl = dataimg._13d;
-      break;
+        break;
       case "13n":
         imgurl = dataimg._13n;
-      break;
+        break;
       case "50d":
         imgurl = dataimg._50d;
-      break;
+        break;
       case "50n":
         imgurl = dataimg._50n;
-      break;
+        break;
     }
-    const image = await fetchimageurl(imgurl);
-    console.log("Downloaded Image");
-    fm.writeImage(base_path+path+".png",image);
+  }
+  const image = await fetchimageurl(imgurl);
+  console.log("Downloaded Image");
+  fm.writeImage(base_path + path + ".png", image);
 }
 
 //get Json weather
@@ -142,19 +146,34 @@ async function fetchWeatherData(url) {
   return res;
 }
 
+async function select_random_bg(id, day_or_night) {
+  const url = "https://github.com/mhrice/frog-weather/raw/main/new-frog.json";
+  const data = await fetchWeatherData(url);
+  if (Number(id) > 800) {
+    if (day_or_night == "night") {
+      id += "n";
+    } else {
+      id += "d";
+    }
+  }
+  images = data.background.id
+  const random = Math.floor(Math.random() * images.length);
+  return images[random];
+}
+
 //start Programming
 
-// Get Location 
+// Get Location
 /*Location.setAccuracyToBest();
 let curLocation = await Location.current();
 console.log(curLocation.latitude);
 console.log(curLocation.longitude);*/
 let unit_id = "";
 let unit_s = "";
-if(UNIT_TYPE=="F"){
+if (UNIT_TYPE == "F") {
   unit_id = "imperial";
   unit_s = "\u2109";
-}else{
+} else {
   unit_id = "metric";
   unit_s = "\u2103";
 }
@@ -168,23 +187,26 @@ const cityName = weatherJSON.name;
 const weatherarry = weatherJSON.weather;
 const iconData = weatherarry[0].icon;
 const weathername = weatherarry[0].main;
+const id = weatherarry[0].id;
 const curTempObj = weatherJSON.main;
 const curTemp = curTempObj.temp;
 const highTemp = curTempObj.temp_max;
 const lowTemp = curTempObj.temp_min;
 const feel_like = curTempObj.feels_like;
 //Completed loading weather data
-
+console.log("ID", id);
 // Widget Background Image
-widget.backgroundImage = Image.fromFile(await fetchimagelocal(iconData+"_bg"));
+path = select_random_bg(id, iconData.includes("n") ? "night" : "day");
+console.log("Path", path);
+widget.backgroundImage = Image.fromFile(await fetchimagelocal(path, true));
 
 //Start Spacing
 widget.addSpacer(0);
 
 //Widget weather Image
-var img = Image.fromFile(await fetchimagelocal(iconData+"_ico"));
+var img = Image.fromFile(await fetchimagelocal(iconData + "_ico", false));
 var widgetimg = widget.addImage(img);
-widgetimg.imageSize = new Size(74,74);
+widgetimg.imageSize = new Size(74, 74);
 widgetimg.rightAlignImage();
 
 // Widget Date
@@ -193,7 +215,7 @@ dateText.textColor = textcolor;
 dateText.font = Font.regularSystemFont(15);
 
 // Widget Weather Temp
-var tempText = widget.addText(Math.round(curTemp)+unit_s);
+var tempText = widget.addText(Math.round(curTemp) + unit_s);
 tempText.textColor = textcolor;
 tempText.font = Font.boldSystemFont(35);
 
